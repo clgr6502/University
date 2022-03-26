ee33484f631cf899776a316f91cb433d1e4eb3d3
Calogero Bellavia <93535463+clgr6502@users.noreply.github.com>
2022-03-26 14:55:23 +0100

    Create cube.js

---
+++ Computer Grafica/Macchina/common/shapes/cube.js
@@ -0,0 +1,32 @@
+///// Definizione cubo
+///// 
+///// Cubo centrato all'origine di dimensioni 2.0 x 2.0 x 2.0 .
+function Cube () {
+
+	this.name = "cube";
+	
+	//Definizione vertici
+	this.vertices = new Float32Array([
+		-1.0, -1.0,  1.0,
+		 1.0, -1.0,  1.0,
+		-1.0,  1.0,  1.0,
+		 1.0,  1.0,  1.0,
+		-1.0, -1.0, -1.0,
+		 1.0, -1.0, -1.0,
+		-1.0,  1.0, -1.0,
+		 1.0,  1.0, -1.0
+	]);
+
+	//Definizione triangoli
+	this.triangleIndices = new Uint16Array([
+		0, 1, 2,  2, 1, 3,  //front
+		5, 4, 7,  7, 4, 6,  //back
+		4, 0, 6,  6, 0, 2,  //left
+		1, 5, 3,  3, 5, 7,  //right
+		2, 3, 6,  6, 3, 7,  //top
+		4, 5, 0,  0, 5, 1   //bottom
+	]);
+	
+	this.numVertices = this.vertices.length/3;
+	this.numTriangles = this.triangleIndices.length/3;	
+}
