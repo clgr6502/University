c173a8894859ebca8979d2fbf3446c611bd750f2
Calogero Bellavia <93535463+clgr6502@users.noreply.github.com>
2022-04-13 10:50:43 +0200

    Update mystrcat.c

--- SOL/Assegnamento1/mystrcat.c
+++ SOL/Assegnamento1/mystrcat.c
@@ -1,65 +1,49 @@
-//Versione copiata a mano
-//possibili typo
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-//Per gli argomenti
 #include <stdarg.h>
-
-const int REALLOC_INC = 16;
-
-void RIALLOCA(char **buf, size_t newsize);
-char *mystrcat(char *buf. size_t sz, char *first, ...);
-
-int main(int argc, char **argv){
-   if(argc < 7){
-      printf("Troppi pochi argomenti\n");
-      exit(1);
-   }
-   char *buffer = NULL; 
-   RIALLOCA(&buffer, REALLOC_INC);
-   
-   buffer[0] = '\0';
-   buffer = mystrcat(buffer, REALLOC_INC, argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], NULL);
-  
-   printf("%s\n", buffer);
-   free(buffer);
-  
-   return 0;
-}
-
-void RIALLOCA(char **buf, size_t newsize){
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+const int REALLOC_INC=16;
+ 
+void RIALLOCA(char** buf, size_t newsize){
    *buf = realloc(*buf, newsize);
-   if(*buf == NULL){
-      printf("Errore memoria terminata\n");
-      exit(-1);
-   }
-}
+}     
+char* mystrcat(char *buf, size_t sz, char *first, ...){
+   va_list ap;
+   va_start(ap, first);
 
-char *mystrcat(char *buf. size_t sz, char *first, ...){
-   va_list va;
-   va_start(va, first);
-  
-   if(sz < strlen(first) + 1){
-      RIALLOCA(&buf, sz + strlen(first) + 1);
-      sz += strlen(first) + 1;
+   char *temp = first;
+
+   if(sz < strlen(buf + 1)){
+      RIALLOCA(&buf, strlen(buf) + strlen(first) + 1 + REALLOC_INC);
+      sz += strlen(first) + 1 + REALLOC_INC;
    }
 
-   char *tmp = malloc(sz);
-   strcpy(buf, first);
-  
-   while((tmp = va_arg(va, char*)) != NULL){
-      if(sz < strlen(buf) + strlen(tmp) + 1){
-         RIALLOCA(&buf, strlen(buf) + strlen(tmp) + 1);
-         sz = strlen(buf) + strlen(tmp) + 1;
+   while(temp != NULL){
+      if(sz < strlen(buf) + 1 + strlen(temp)){
+         RIALLOCA(&buf, strlen(temp) + strlen(buf) + 1 + REALLOC_INC);
+         sz = strlen(buf) + strlen(temp) + 1 + REALLOC_INC;
       }
-      int j = 0;
-      for(int i = strlen(buf); i < strlen(buf) + strlen(tmp); i++){
-         buf[i] = tmp[j];
-         j++;
-      }
+      strcpy(buf + strlen(buf), temp);
+      temp = va_arg(ap, char *);
    }
-   va_end(va);
+
+   va_end(ap);
    return buf;
+}
+ 
+int main(int argc, char *argv[]) {
+   if(argc < 7){
+      printf("troppi pochi argomenti\n");
+      return -1;
+   }
+   char *buffer = NULL;
+
+   RIALLOCA(&buffer, REALLOC_INC);  // macro che effettua l'allocazione del 'buffer'
+   buffer[0]='\0'; // mi assicuro che il buffer contenga una stringa
+
+   buffer = mystrcat(buffer, REALLOC_INC, argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], NULL);
+   printf("%s\n", buffer);     
+   free(buffer);
+   return 0;
 }
