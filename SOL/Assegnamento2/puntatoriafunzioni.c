e8b8004106ea73a2378d37451ae777ab2db5f8bb
Calogero Bellavia <93535463+clgr6502@users.noreply.github.com>
2022-04-19 19:14:50 +0200

    Update puntatoriafunzioni.c

--- SOL/Assegnamento2/puntatoriafunzioni.c
+++ SOL/Assegnamento2/puntatoriafunzioni.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #define OPT 4
 
@@ -12,42 +12,41 @@ int arg_h(const char *arg);
 
 long isNumber(const char *s);
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv){
    if(argc == 1){
       printf("Troppi pochi argomenti\n");
       exit(1);
    }
-  
-   /*TIPO (nome*[N])(argomenti) = {f1, f2, fn};*/
-   int(v*[OPT])(const char *arg) = {arg_h, arg_m, arg_n, arg_o};
+
+
+   /*TIPO (nome*[N])(argomenti) = {f1, f2, ...,fn};*/
+   int (*funz[4])(const char *) = {arg_h, arg_m, arg_n, arg_o};
    int opt;
- 
-   while ((opt = getopt(argc,argv, "n:m:o:h")) != -1) {
-      switch(opt) {
+
+
+   while((opt = getopt(argc, argv, "n:m:o:h")) != -1){
+      switch(opt){
          case '?':
-            printf("Parametro sconosciuto\n");
+            printf("Parametro non riconosciuto\n");
          break;
          default:
          // invocazione della funzione di gestione passando come parametro l'argomento restituito da getopt
-            if (V[opt%4]( (optarg==NULL ? argv[0] : optarg) ) == -1) {
-               printf("Invalid argument\n");
-               exit(1);
+            if(funz[opt % OPT]((optarg == NULL ? argv[0] : optarg))== -1){
+               printf("Errore parametro ");
             }
          break;
       }
+      printf("\n");
    }
-   printf("\n");
-   return 0; 
+   return 0;
 }
 
-
 int arg_n(const char *arg){
-   long n = isNumber(arg);
-   if(n != -1){
-      printf("%ld ", n);
+   long num;
+   if((num = isNumber(arg)) != -1){
+      printf("%ld ", num);
       return 0;
    }else{
-      printf("Not a number ");
       return -1;
    }
 }
@@ -58,7 +57,6 @@ int arg_m(const char *arg){
 
 int arg_o(const char *arg){
    if(arg == NULL || arg[0] == '\0'){
-      printf("Stringa vuota\n");
       return -1;
    }else{
       printf("%s ", arg);
@@ -67,11 +65,11 @@ int arg_o(const char *arg){
 }
 
 int arg_h(const char *arg){
-   printf("TODO: Messaggio di aiuto\n");
+   printf("Utilizzo: -n <num> -m <num> -o <string> -h ");
    return 0;
 }
 
-long isNumber(const char* s){
+long isNumber(const char* s) {
    char* e = NULL;
    long val = strtol(s, &e, 0);
    if (e != NULL && *e == (char)0) return val; 
