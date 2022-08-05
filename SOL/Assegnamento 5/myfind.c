686f8945c63d3e2bbeb9bc330c4ca9585a02ea47
Calogero Bellavia <93535463+clgr6502@users.noreply.github.com>
2022-08-05 11:16:02 +0200

    Update and rename myfind_v1.c to myfind.c

--- SOL/Assegnamento 5/myfind.c
+++ SOL/Assegnamento 5/myfind.c
@@ -6,16 +6,18 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <time.h>
 #include <error.h>
 
 #define MAX 128
 
-void myfind(char *dir, char *file, char *path, int *flag);
+void myfind(char *dir, char *file, char *path, int *flag, char *lc);
 int isFile(struct stat buf);
 int isDir(struct stat buf);
 
 int main(int argc, char **argv){
    int flag = 0;
+   char *lc = malloc(MAX);
 
    if(argc < 3){
       printf("Troppi pochi argomenti\n");
@@ -26,74 +28,84 @@ int main(int argc, char **argv){
 
    for(int i = 2; i < argc; i++){
       flag = 0;
-      myfind(argv[1], argv[i], path, &flag);
+      myfind(argv[1], argv[i], path, &flag, lc);
 
+      printf("----------\n");
       if(flag){
          printf("Trovato %s: %s\n", argv[i], path);
+         printf("Ultima modifica: %s", lc);
       }else{
          printf("%s non trovato\n", argv[i]);
       }
-      printf("------------\n");
    }
 
    free(path);
+   free(lc);
 
    return 0;
 }
 
-void myfind(char *dir, char *file, char *path, int *flag){
+void myfind(char *dir, char *file, char *path, int *flag, char *lc){
    //Per caricare i dati del file/directory
    struct stat stbuf;
    struct stat st;
 
-printf("<%s>\n", dir);
    char *atDir = malloc(MAX);
-   sprintf(atDir, "%s/%s", getcwd(atDir, MAX), dir);
 
-printf("[%s]\n", atDir);
-   if(stat(atDir, &stbuf) == -1){
+   if(stat(dir, &stbuf) == -1){
       perror("Errore");
       exit(1);
    }
 
    char *nome = malloc(MAX);
    char *perc = malloc(MAX);
+   char *prev = malloc(MAX);
 
    DIR *dfd;
    struct dirent *dp;
 
-   if((dfd = opendir(atDir)) == NULL){
+   if((dfd = opendir(dir)) == NULL){
       perror("Impossibile aprire la directory");
       exit(1);
    }
 
+   chdir(dir);
+
    while((dp = readdir(dfd)) != NULL){
       strcpy(nome, dp->d_name);
-      sprintf(perc, "%s/%s", dir, nome);
+      getcwd(atDir, MAX);
 
-      if(stat(perc, &st) != -1){
+      if(stat(nome, &st) != -1){
          if(strcmp(nome, "..") == 0 ||
             strcmp(nome,  ".") == 0){
             continue;
          }else if(isDir(st)){
-            sprintf(perc, "%s/%s", atDir, nome);
-printf("%s\n", perc);
-chdir(perc);
-            myfind(nome, file, path, flag);
+            myfind(nome, file, path, flag, lc);
          }else{
             if(strcmp(nome, file) == 0){
                *flag = 1;
+               sprintf(lc, "%s", ctime(&st.st_mtime));
                sprintf(path, "%s/%s", atDir, file);
+
+               chdir("..");
+               closedir(dfd);
+               free(nome);
+               free(perc);
+               free(atDir);
+               free(prev);
+
                return;
             }
          }
       }
    }
+   chdir("..");
 
    closedir(dfd);
    free(nome);
    free(perc);
    free(atDir);
+   free(prev);
 
    return;
 }
